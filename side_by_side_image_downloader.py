c2f875efc1987d985518e5144918a488ec38aebf
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2022-06-27 20:03:37 -0500

    have it working for double spreads reading left and right

--- side_by_side_image_downloader.py
+++ side_by_side_image_downloader.py
@@ -28,7 +28,7 @@ def main():
         if re.search('^http|^https', image_to_download):
             last_slash = image_to_download.rindex('/')
             file_name = image_to_download[last_slash + 1::]
-            subprocess.run(['wget', '-O', folder_name + '/' + file_name, image_to_download])
+            #subprocess.run(['wget', '-O', folder_name + '/' + file_name, image_to_download])
             images_to_convert.append(file_name)
 
 
@@ -39,12 +39,47 @@ def main():
     rgb_images = []
     for image in images:
         rgb_images.append(image.convert('RGB'))
-    pdf_path = "./" + folder_name + ".pdf"
 
-    rgb_images[0].save(
-            pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=rgb_images[1::]
-    )
+    pdf_path = "double_spread_testing" + ".pdf"
 
-    subprocess.run(['rm', '-rf', folder_name + '/'])
+    iter_max = len(rgb_images)
+    rgb_images[0].save(f"testing/image_0.jpg")
+    i = 1
+    combined_images = []
+    while i < iter_max:
+        print(i)
+        max_height = 0
+        total_width = 0
+        try:
+            if rgb_images[i].size[0] > 1400 or i == iter_max - 1:
+                print('beep')
+                #new_img = Image.new('RGB', (rgb_images[i].size[0], rgb_images[i].size[1]))
+                #new_img.paste(rgb_images[i], (0,0))
+                rgb_images[i].save(f"testing/image_{i}.jpg")
+                i += 1
+            elif i < iter_max - 1 and rgb_images[i].size[0] < 1300 and rgb_images[i+1].size[0] < 1300:
+                image_array = [rgb_images[i + 1], rgb_images[i]]
+                total_width = 0
+                max_height = 0
+                # find the width and height of the final image
+                for img in image_array:
+                    total_width += img.size[0]
+                    max_height = max(max_height, img.size[1])
+
+                new_img = Image.new('RGB', (total_width, max_height))
+                current_width = 0
+                for img in image_array:
+                    new_img.paste(img, (current_width,0))
+                    current_width += img.size[0]
+                new_img.save(f"testing/image_{i}.jpg")
+                combined_images.append(new_img)
+                i += 2
+        except:
+            print('bad')
+
+
+#    combined_images[0].save( pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=combined_images[1::])
+
+    #subprocess.run(['rm', '-rf', folder_name + '/'])
 if __name__ == '__main__':
     main()
