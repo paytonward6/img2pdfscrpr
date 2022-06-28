75fddc56c5e1028628f755db1b4eea849f494abb
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2022-06-28 12:29:23 -0500

    commented code

--- side_by_side_image_downloader.py
+++ side_by_side_image_downloader.py
@@ -6,56 +6,73 @@ import subprocess
 import os
 from PIL import Image
 import re
+import traceback
 
 def main():
-    #url = str(input("Input a URL: "))
-    url = "https://onepiecechapters.com/chapters/2305/one-piece-chapter-1052"
-
+    url = str(input("Input a URL: "))
+    
+    """
+    Get final path of the URL make that the name of the folder
+    (also the name of the PDF later on)
+    """
     last_slash = url.rindex('/')
     folder_name = url[last_slash + 1::]
-    subprocess.run(['mkdir', folder_name])
+    
+    """
+    Create a directory and a subdirectory to put the images to download in
+    """
+    subprocess.run(['mkdir', '-p', folder_name + '/' + "combined"])
 
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
-
+    
+    
+    #Obtain all img tags at the specified URL
     image_links = soup.find_all("img")
 
     images_to_convert = []
     for image in image_links:
 
+        #Obtain the URL of the image on in each img tag
         image_to_download = image["src"]
+        
+        #Only want the URLs that begin with http or https
         if re.search('^http|^https', image_to_download):
             last_slash = image_to_download.rindex('/')
             file_name = image_to_download[last_slash + 1::]
-            #subprocess.run(['wget', '-O', folder_name + '/' + file_name, image_to_download])
+            subprocess.run(['wget', '-O', folder_name + '/' + file_name, image_to_download])
             images_to_convert.append(file_name)
 
-
+    #Create a list of single Image objects (not combined side by side)
     images = []
     for f in images_to_convert:
         images.append(Image.open('./' + folder_name + '/' + f))
 
+    #Removes 'RGBA' warning
     rgb_images = []
     for image in images:
         rgb_images.append(image.convert('RGB'))
-
-    pdf_path = "double_spread_testing" + ".pdf"
-
+    
     iter_max = len(rgb_images)
-    rgb_images[0].save(f"testing/image_0.jpg")
+    file_name = folder_name + '/combined/image_0.jpg'
+    combined_images = [file_name]
+
+    """ For for manga, oftentimes the first page, so we
+        it on it's own page
+    """
+    if len(rgb_images) != 0:
+        rgb_images[0].save(file_name)
     i = 1
-    combined_images = []
     while i < iter_max:
-        print(i)
         max_height = 0
         total_width = 0
         try:
+            # If the width is greater than 1400, put that image on it's own page
             if rgb_images[i].size[0] > 1400 or i == iter_max - 1:
-                print('beep')
-                #new_img = Image.new('RGB', (rgb_images[i].size[0], rgb_images[i].size[1]))
-                #new_img.paste(rgb_images[i], (0,0))
-                rgb_images[i].save(f"testing/image_{i}.jpg")
+                file_name = folder_name + "/combined/image_" + str(i) + ".jpg"
+                rgb_images[i].save(file_name)
+                combined_images.append(file_name)
                 i += 1
             elif i < iter_max - 1 and rgb_images[i].size[0] < 1300 and rgb_images[i+1].size[0] < 1300:
                 image_array = [rgb_images[i + 1], rgb_images[i]]
@@ -68,18 +85,30 @@ def main():
 
                 new_img = Image.new('RGB', (total_width, max_height))
                 current_width = 0
+                file_name = folder_name + "/combined/image_" + str(i) + ".jpg"
+                # Create a new image, combinding two images side by side
                 for img in image_array:
                     new_img.paste(img, (current_width,0))
                     current_width += img.size[0]
-                new_img.save(f"testing/image_{i}.jpg")
-                combined_images.append(new_img)
+                new_img.save(file_name)
+                combined_images.append(file_name)
                 i += 2
         except:
-            print('bad')
+            traceback.print_exc()
+    #Write the opened combined Images to a new list
+    images = []
+    for f in combined_images:
+        images.append(Image.open(f))
 
+    pdf_path = "./" + folder_name + ".pdf"
+    
+    #Save the combined images in a new pdf
+    images[0].save(
+            pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1::]
+    )
 
-#    combined_images[0].save( pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=combined_images[1::])
+    #Cleanup images in directory
+    subprocess.run(['rm', '-rf', folder_name])
 
-    #subprocess.run(['rm', '-rf', folder_name + '/'])
 if __name__ == '__main__':
     main()
