ba7a5b9ab0a37f10d5a332fab6506ac76d44ab23
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2022-08-11 18:19:37 -0500

    removed old files from repo

--- test/test_url.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from img2pdfscrpr.img2pdfscrpr import ImageDownloader
-
-temp = ImageDownloader()
-
-def test_get_url():
-    assert temp.parse_url('https://trufflewufflepigs.com/PidAndCorn.jpeg/') =='https://trufflewufflepigs.com/PidAndCorn.jpeg/'
